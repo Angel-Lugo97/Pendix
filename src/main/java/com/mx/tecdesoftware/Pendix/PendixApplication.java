3e437d8d5fa81793fd8a16ca840ad80c14ad937b
Angel Lugo <angel.lugo@tecdesoftware.edu.mx>
2026-07-20 20:02:05 -0600

    Add: upload Pendix the base code

---
+++ src/main/java/com/mx/tecdesoftware/Pendix/PendixApplication.java
@@ -0,0 +1,13 @@
+package com.mx.tecdesoftware.Pendix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PendixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PendixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mx/tecdesoftware/Pendix/PendixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.tecdesoftware.Pendix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PendixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

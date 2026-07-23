8f0cd83cd6e728430d4b0331dd47a171bef68927
Angel Lugo <angel.lugo@tecdesoftware.edu.mx>
2026-07-23 09:47:55 -0600

    feat(domain): add project and task models

---
+++ src/main/java/com/mx/tecdesoftware/Pendix/domain/Project.java
@@ -0,0 +1,80 @@
+package com.mx.tecdesoftware.Pendix.domain;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Project {
+
+    private Integer projectId;
+    private Integer ownerId;
+    private String name;
+    private String description;
+    private LocalDateTime startDate;
+    private LocalDateTime dueDate;
+    private String state;
+    private List<Task> tasks;
+
+    public Integer getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(Integer projectId) {
+        this.projectId = projectId;
+    }
+
+    public Integer getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Integer ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDateTime dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+}
---
+++ src/main/java/com/mx/tecdesoftware/Pendix/domain/Task.java
@@ -0,0 +1,88 @@
+package com.mx.tecdesoftware.Pendix.domain;
+
+import java.time.LocalDateTime;
+
+public class Task {
+
+    private Integer taskId;
+    private Integer projectId;
+    private Integer assignedUserId;
+    private String title;
+    private String description;
+    private LocalDateTime creationDate;
+    private LocalDateTime dueDate;
+    private String priority;
+    private String state;
+
+    public Integer getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(Integer taskId) {
+        this.taskId = taskId;
+    }
+
+    public Integer getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(Integer projectId) {
+        this.projectId = projectId;
+    }
+
+    public Integer getAssignedUserId() {
+        return assignedUserId;
+    }
+
+    public void setAssignedUserId(Integer assignedUserId) {
+        this.assignedUserId = assignedUserId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public LocalDateTime getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDateTime dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
